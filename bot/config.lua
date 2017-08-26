ed91d7e9d6b22331ffd327bbe97b3d43ad69fc90
Dev_astaff <astaff144@gmail.com>
2017-08-26 18:57:51 +0300

    testing1

---
+++ bot/config.lua
@@ -0,0 +1,40 @@
+do local _ = {
+  enabled_plugins = {
+    "addrem",
+    "bc",
+    "supergroup",
+    "msg_checks",
+    "pin",
+    "owner",
+    "online",
+    "plugins",
+    "admin",
+    "id",
+    "del",
+    "clean",
+    "expiretime",
+    "filter",
+    "setlink",
+    "setrules",
+    "me",
+    "like",
+    "rmsg_all",  
+    "leave"  
+  },
+  group  = {
+    data = "bot/group.json"
+  },
+  sudo_users = {
+    202020202, --sudo ایدي المطور
+    0,
+    0
+  },
+robot = {
+    010101010,--bot ایدي البوت
+    0
+  }
+}
+return _
+end
+
+--DEV_ASTAFF-V3
