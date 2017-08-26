5e87fa9e7221553124ec7c917c09937cbd5cc35f
Dev_astaff <astaff155@users.noreply.github.com>
2017-08-26 19:46:59 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 ./tg/tg -s ./bot/bot.lua
-echo "\033[38;5;110m
-██╗  ██╗████████╗██████╗ ██╗ █████╗
-╚██╗██╔╝╚══██╔══╝██╔══██╗██║██╔══██╗
- ╚███╔╝    ██║   ██████╔╝██║███████║
- ██╔██╗    ██║   ██╔══██╗██║██╔══██║
-██╔╝ ██╗   ██║   ██║  ██║██║██║  ██║
-╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝
-\033[0m\n"
+ echo -e "\033[38;5;208m"
+   echo -e " > Channel   : @DEV_ASTAFF                  \e[36m"
+   echo -e " > Developer : @ASTAFF                      \e[36m"
+   echo -e " > Bot ID    : @asdbasra                    \e[36m" 
+   echo -e " > Github : Github.com/astaff155/DEV_ASTAFF \e[36m"
+   echo -e "                                            \e[36m"
+   echo -e "                                            \e[36m"
 exit
 
-# @mafia_cli
-# https://github.com/mafia-007
+# DEV_ASTAFF-V3
